906efdc70cb3efb39365235eb306436718429ca7
Bibin C Ranjith <bibincrajith@gmail.com>
2023-12-07 19:24:57 +0530

    netflix clone

---
+++ src/components/Row.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react'
+import instance from '../instance';
+import './Row.css'
+function Row({title, fetchUrl, isPoster}){
+   /* console.log(fetchUrl);*/
+   const [allMovies , setAllMovies]  = useState()
+   const base_url = "https://image.tmdb.org/t/p/original/";
+
+    const fetchData = async ()=>{
+       const {data} = await instance.get(fetchUrl)
+        setAllMovies(data.results);
+    }
+    console.log(allMovies);
+
+    useEffect(() => {
+        fetchData()
+    },[])
+
+return (
+<div className='row'> 
+    <h1>{title}</h1>
+    <div className="movie-row">
+        {
+            allMovies?.map(item =>(
+                <img className={`movie ${isPoster && 'movie_poster'}`} src={`${base_url}${isPoster?item.poster_path:item.backdrop_path}`} alt="no image" />
+            ))
+        }
+    </div>
+    </div>
+    
+)
+}
+export default Row
